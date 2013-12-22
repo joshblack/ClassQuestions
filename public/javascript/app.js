573feca018fc156658a0d2f1780ebfa796cfb5c8
joshuatblack <joshuatblack@ufl.edu>
2013-12-22 17:04:11 -0500

    fixing up styling issues

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,8 @@
-jQuery(function($) {
-	var socket = io.connect();
-	var $questionForm = $('#question-submit'), 
+"use strict";
+
+jQuery(function ($) {
+	var socket = io.connect(),
+        $questionForm = $('#question-submit'), 
 		$questionBox = $('#question'), 
 		$users = $('#users'), 
 		$questions = $('#questions-display'), 
@@ -9,11 +11,11 @@ jQuery(function($) {
 		$usernameBox = $('#username');
 
 	// First step is to handle our username form
-	$usernameForm.submit(function(e) {
+	$usernameForm.submit(function (e) {
 		e.preventDefault();
 
 		// create a new socket event called create_user
-		socket.emit('create_user', $usernameBox.val(), function(data) {
+		socket.emit('create_user', $usernameBox.val(), function (data) {
 			// If we've created a valid user we hide the username form and display our questions
 			if (data) {
 				$('#username-wrapper').hide();
@@ -26,10 +28,10 @@ jQuery(function($) {
 	});
 
 	// Now we need to handle the case where we receive an updated list of users
-	socket.on('usernames', function(data) {
+	socket.on('usernames', function (data) {
 		// Have an empty string of html that we append with all our users
-		var html = '';
-		for(i = 0; i < data.length; i++) {
+		var html = '', i;
+		for (i = 0; i < data.length; i++) {
 			html += data[i] + '<br/>';
 		}
 		// Replaces current value of $users with our modified string of html
@@ -37,14 +39,14 @@ jQuery(function($) {
 	});
 
 	// For handling question submissions we define a send_question socket event
-	$questionForm.submit(function(e) {
+	$questionForm.submit(function (e) {
 		e.preventDefault();
 		socket.emit('send_question', $questionBox.val());
 		$questionBox.val('');
 	});
 
 	// When we receive a new question we just append to our list of questions 
-	socket.on('new_question', function(data) {
+	socket.on('new_question', function (data) {
 		$questions.append('<b>' + data.username + ': </b>' + data.question + "<br/>");
 	});
 
